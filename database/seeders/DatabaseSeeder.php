9dda05e82e99f84cbf8a7efd4ff2390469140b43
nahidferdous <nahidferdous240@gmail.com>
2025-01-29 17:14:22 +0600

    !

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         User::firstOrCreate([
             'name' => 'Admin',
-            'email' => 'it@alazzaz.help',
+            'email' => 'dev@fazit.sa',
             'email_verified_at' => now(),
             'password' => Hash::make('12345678'),
             'role' => 'admin',
