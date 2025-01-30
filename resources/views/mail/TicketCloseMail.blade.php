91285b69be59310df408a67895445f09f3b00025
nahidferdous <nahidferdous240@gmail.com>
2025-01-30 12:49:53 +0600

    !

--- resources/views/mail/TicketCloseMail.blade.php
+++ resources/views/mail/TicketCloseMail.blade.php
@@ -126,7 +126,7 @@
     <div class="mailContainer">
         <div class="logoContainer">
             {{--            <img class="logoHead" src="{{url('/images/logo.webp')}}" alt="">--}}
-            <img class="logoHead" src="https://alazzaz.tech/logo.png" alt="" width="200">
+            <img class="logoHead" src="{{ env('APP_FRONTEND_URL') }}/logo.png" alt="" width="200">
         </div>
         <div>
             <h2>Dear {{$user?->name}},</h2>
